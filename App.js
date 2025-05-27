29055b26e72cd9788ad7d225b29b61ed07c7a880
vikta2097 <thigamwangi2027@gmail.com>
2025-05-27 12:59:31 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import AuthForm from "./components/AuthForm";
+function App() {
+  return (
+     <div className="App">
+      <AuthForm />
+    </div>
+  );
+}
+
+export default App;
